f479c2320a3bd312b3856dee0c164e6771ae237f
NikiforovAll <nik-alekcey@yandex.ua>
2016-03-13 13:49:31 +0200

    junk commit

--- AnalyticHierarchyProcess/Program.cs
+++ AnalyticHierarchyProcess/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AnalyticHierarchyManager;
+using AnalyticHierarchyManager = AnalyticHierarchyManager.AnalyticHierarchyManager;
 
 namespace AnalyticHierarchyProcess
 {
@@ -10,6 +12,7 @@ namespace AnalyticHierarchyProcess
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
